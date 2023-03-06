62aae7a858f13714fc38c007a3f5b8a2c11098e2
sweytha <sweythak@gmail.com>
2023-03-06 10:07:51 -0500

    spring starter project

---
+++ src/test/java/com/bptn/FeedApplication/FeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bptn.FeedApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d9b6df92a19c9f257f03e1a5115f8fc97f82fe9e
sweytha <sweythak@gmail.com>
2023-03-10 11:26:18 -0500

    friday codes

--- src/main/java/com/bptn/FeedApplication/service/UserService.java
+++ src/main/java/com/bptn/FeedApplication/service/UserService.java
@@ -19,6 +19,10 @@ public class UserService {
 	@Autowired
 	UserRepository userRepository;
 
+	
+	@Autowired
+	EmailService emailService;
+	
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
 	}
@@ -39,6 +43,8 @@ public class UserService {
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
 		this.userRepository.save(user);
+		
+		this.emailService.sendVerificationEmail(user);
 		return user;
 		
 	}

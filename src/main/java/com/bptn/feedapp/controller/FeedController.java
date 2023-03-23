1bb4297163df96c74b41301bf29e23e72893b4b2
sweytha <sweythak@gmail.com>
2023-03-23 15:33:23 -0400

    thursday codes week 8

---
+++ src/main/java/com/bptn/feedapp/controller/FeedController.java
@@ -0,0 +1,61 @@
+package com.bptn.feedapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bptn.feedapp.domain.PageResponse;
+import com.bptn.feedapp.jpa.Feed;
+import com.bptn.feedapp.service.FeedService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/feeds")
+public class FeedController {
+	
+	
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	FeedService feedService;
+	
+	@PostMapping
+	public Feed createFeed(@RequestBody Feed feed) {
+				
+		logger.debug("Creating Feed");
+				
+		return this.feedService.createFeed(feed);
+	}
+	
+	@GetMapping("/{feedId}")
+	public Feed getFeed(@PathVariable int feedId) {
+			
+		logger.debug("Getting Feed, feedId: {}", feedId);
+			
+		return this.feedService.getFeedById(feedId);	
+	}
+	
+	@GetMapping("/user/{pageNum}/{pageSize}")
+	public PageResponse<Feed> getUserFeeds(@PathVariable int pageNum, @PathVariable int pageSize) {
+			
+		logger.debug("Getting User Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+			
+		return this.feedService.getUserFeeds(pageNum, pageSize);	
+	}
+	
+	
+	@GetMapping("/other/{pageNum}/{pageSize}")
+	public PageResponse<Feed> getOtherUsersFeeds(@PathVariable int pageNum, @PathVariable int pageSize) {
+			
+		logger.debug("Getting Other Users Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+			
+		return this.feedService.getOtherUsersFeeds(pageNum, pageSize);	
+	}
+}
---
+++ src/main/java/com/bptn/feedapp/service/FeedService.java
@@ -0,0 +1,78 @@
+package com.bptn.feedapp.service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedapp.domain.PageResponse;
+import com.bptn.feedapp.exception.domain.FeedNotFoundException;
+import com.bptn.feedapp.exception.domain.UserNotFoundException;
+import com.bptn.feedapp.jpa.Feed;
+import com.bptn.feedapp.jpa.User;
+import com.bptn.feedapp.repository.FeedRepository;
+import com.bptn.feedapp.repository.UserRepository;
+
+@Service
+public class FeedService {
+
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	FeedRepository feedRepository;
+
+	public Feed createFeed(Feed feed) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		User user = this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+		feed.setUser(user);
+		feed.setCreatedOn(Timestamp.from(Instant.now()));
+
+		return this.feedRepository.save(feed);
+	}
+
+	public Feed getFeedById(int feedId) {
+
+		return this.feedRepository.findById(feedId)
+				.orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+	}
+
+	public PageResponse<Feed> getUserFeeds(int pageNum, int pageSize) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		User user = this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+		Page<Feed> paged = this.feedRepository.findByUser(user,
+				PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
+
+		return new PageResponse<Feed>(paged);
+	}
+
+	public PageResponse<Feed> getOtherUsersFeeds(int pageNum, int pageSize) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+		Page<Feed> paged = this.feedRepository.findByUserNot(user,
+				PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
+
+		return new PageResponse<Feed>(paged);
+	}
+
+}
